3725bbf46fab6695b7b0af374d67980cfea2d868
SinuheJardinez <sinuhe.jh@gmail.com>
2021-12-14 17:54:33 -0600

    conectado server-client: solicitud de guitars

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 //import
 const express = require ("express")
 const app =  express()
+const cors = require("cors")
+
 require ("dotenv").config()
 const connectDB = require ('./config/db')
 
 //Middlewares
 connectDB()
+//habilitamos cors(accesos de ambientes de desarrollo de terceros)
+app.use(cors())
+
 //todas las peeticiones y respuestas se manejan en protocolo JSON
 app.use(express.json())
 
